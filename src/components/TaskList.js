57098942579978bfa26bc56f515cc6c9ed3df706
Cheryl Mbani <mbanicheryl45@gmail.com>
2025-07-13 08:36:49 +0300

    displaying items

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,8 +1,17 @@
 import React from "react";
+import Task from "./Task.js"
 
-function TaskList() {
+function TaskList({tasks}) {
+  const taskItems=tasks.map((task, index)=>{
+    return (
+    <Task 
+    key={index}
+    text={task.text}
+    category={task.category}/>
+    );
+  });
   return (
-    <div className="tasks">
+    <div className="tasks">{taskItems}
       {/* display a list of tasks using Task component */}
     </div>
   );
