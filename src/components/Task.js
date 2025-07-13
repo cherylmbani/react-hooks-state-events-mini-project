837bb00564501b3bd98e7275e7415e5d9b413b23
Cheryl Mbani <mbanicheryl45@gmail.com>
2025-07-13 09:09:51 +0300

    adding click event ad function handler

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-function Task({key, text, category}) {
+function Task({text, category, onDelete}) {
 
   
   return (
     <div className="task" >
       <div className="label">{category}</div>
       <div className="text">{text}</div>
-      <button className="delete">X</button>
+      <button className="delete" onClick={()=>onDelete(text)}>X</button>
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import Task from "./Task.js"
 
 function TaskList({tasks}) {
-  const taskItems=tasks.map((task, index)=>{
+  const [taskList, setTaskList] = useState(tasks);
+
+  function handleDeleteTask(deletedTask){
+    const updatedList=taskList.filter((task)=>
+      task.text!==deletedTask);
+      setTaskList(updatedList);
+    
+  }
+  const taskItems=taskList.map((task, index)=>{
     return (
     <Task 
     key={index}
     text={task.text}
-    category={task.category}/>
+    category={task.category}
+    onDelete={handleDeleteTask}
+    />
     );
   });
   return (
